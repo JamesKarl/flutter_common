41d6430aead327715c49111abf392f3ea90a2843
JamesKarl <jameskarl2@163.com>
2020-04-29 12:34:44 +0800

    add flutter common

--- lib/fluttercommon.dart
+++ lib/fluttercommon.dart
@@ -1,1 +1,38 @@
 library fluttercommon;
+
+export 'src/extentions/string_extentions.dart';
+export 'src/extentions/datetime_extensions.dart';
+export 'src/extentions/num_extentions.dart';
+export 'src/extentions/build_context_extentions.dart';
+
+export 'src/http/http_wrapper.dart';
+export 'src/http/http_configs.dart';
+export 'src/http/file_upload.dart';
+
+export 'src/picker.dart';
+
+export 'src/platform/platform_apis.dart';
+
+export 'src/widgets/action_buttons.dart';
+export 'src/widgets/avatar.dart';
+export 'src/widgets/bottom_one_button.dart';
+export 'src/widgets/common_future_builder.dart';
+export 'src/widgets/dash_circle_divider_widget.dart';
+export 'src/widgets/dash_line_widget.dart';
+export 'src/widgets/date_navigator_widget.dart';
+export 'src/widgets/dot_text_widget.dart';
+export 'src/widgets/from_to_widget.dart';
+export 'src/widgets/full_width_button.dart';
+export 'src/widgets/http_request_empty.dart';
+export 'src/widgets/http_request_failed.dart';
+export 'src/widgets/http_request_loading.dart';
+export 'src/widgets/key_value_selector_widget.dart';
+export 'src/widgets/key_value_widget.dart';
+export 'src/widgets/lazy_widget.dart';
+export 'src/widgets/markers.dart';
+export 'src/widgets/navigation_item_widget.dart';
+export 'src/widgets/paged_list_view.dart';
+export 'src/widgets/photo_widget.dart';
+export 'src/widgets/seal_widget.dart';
+export 'src/widgets/send_sms_widget.dart';
+export 'src/widgets/simple_calendar_widget.dart';
